a67fbc4def1ea2a06f61ef4171bd0b94da681e08
bushraYounes <boushra.ali.younes@gmail.com>
2024-03-17 22:18:01 +0300

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/FrontEnd_Task1_villa_website'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
